778afeca70a358e9a6cc237f77b37b7ca370ac46
Mike Burnside <mburnside@.salesforce.com>
2018-01-16 09:41:29 +1100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const path = require('path')
+
+const request = require('request-promise')
+
 const PORT = process.env.PORT || 5000
+
+
+
 var jwt = require('jsonwebtoken');
+
 
 var cert=process.env.PEM;
 
@@ -9,7 +16,12 @@ var cert=process.env.PEM;
 var token = jwt.sign({ sub:'heroku@56OzeWE7XXLnUutMQeYOGq9ER1489468256214',aud:'https://api.einstein.ai/v2/oauth2/token',exp: Math.floor(Date.now() / 1000) + (60 * 60) }, cert, { algorithm: 'RS256'});
 console.log(token);
 
-
+const options = {
+  method: 'POST',
+  uri: 'https://api.einstein.ai/v2/oauth2/token?grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion='+token,
+  json: true 
+    // JSON stringifies the body automatically
+}
 var app = express();
 
 // view engine setup
@@ -21,12 +33,23 @@ app.use(express.static('public'));
 app.set('port', (process.env.PORT || 5000));
 
 
-app.get('/', function(request, response) { 
-  response.render('pages/index');
+app.get('/', function(req, resp) { 
+  resp.render('pages/index');
 });
 
-app.get('/einstein', function(request, response) { 
-	response.send(token);
+app.get('/einstein', function(req, resp) { 
+
+request(options)
+  .then(function (response) {
+  	console.log(response);
+    // Handle the response
+  })
+  .catch(function (err) {
+    console.log(err);
+  })
+
+
+	resp.send(token);
   //response.render('pages/einstein');
 });
 

b06cc2b678a07a1d26986e33b8300ba6aa857253
Mike Burnside <mburnside@salesforce.com>
2023-06-30 11:19:39 +1000

    first

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
-const express = require('express')
-const path = require('path')
-const request = require('request-promise');
+const express = require("express");
+const path = require("path");
+const request = require("request-promise");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
+var jwt = require("jsonwebtoken");
 
+var cert = process.env.PEM;
 
-var jwt = require('jsonwebtoken');
-
-
-var cert=process.env.PEM;
-
-  // get private key
+// get private key
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
+app.set("port", process.env.PORT || 5000);
 
-app.set('port', (process.env.PORT || 5000));
-
-
-app.get('/', function(req, resp) { 
-  resp.render('pages/index');
+app.get("/", function (req, resp) {
+  resp.render("pages/index");
 });
 
-app.get('/getaccesstoken', function(req, resp) { 
+app.get("/getaccesstoken", function (req, resp) {
+  var token = jwt.sign(
+    {
+      sub: "heroku-anz-1912602@herokumanager.com",
+      aud: "https://api.einstein.ai/v2/oauth2/token",
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    },
+    cert,
+    { algorithm: "RS256" }
+  );
+  console.log(token);
 
-var token = jwt.sign({ sub:'heroku@56OzeWE7XXLnUutMQeYOGq9ER1489468256214',aud:'https://api.einstein.ai/v2/oauth2/token',exp: Math.floor(Date.now() / 1000) + (60 * 60) }, cert, { algorithm: 'RS256'});
-console.log(token);
-
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-type':'application/x-www-form-urlencoded'
-   
-  },
-  form: {
-    grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-    scope:'offline',
-    assertion:token
-  },
-  uri: 'https://api.einstein.ai/v2/oauth2/token',
-  json: true 
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    form: {
+      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+      scope: "offline",
+      assertion: token,
+    },
+    uri: "https://api.einstein.ai/v2/oauth2/token",
+    json: true,
     // JSON stringifies the body automatically
-}
+  };
 
+  request(options)
+    .then(function (response) {
+      console.log(response);
+      resp.setHeader("Content-Type", "application/json");
+      resp.send(response);
+      //Handle the response
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 
-request(options)
-  .then(function (response) {
-  	console.log(response);
-  	resp.setHeader('Content-Type', 'application/json');
-  	resp.send(response);
-    //Handle the response
-  })
-  .catch(function (err) {
-    console.log(err);
-  })
-
-
-	
   //response.render('pages/einstein');
 });
 
-app.listen(app.get('port'), function() {
-  console.log(`Listening on ${ PORT }`)
-})
+app.listen(app.get("port"), function () {
+  console.log(`Listening on ${PORT}`);
+});
